af52faba9c21e4230ec86ca6b81b69c16ec98d09
Alysha Huot <alyshajhuot@gmail.com>
2023-11-03 08:09:37 -0600

    complete sum-from-a-to-b function

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+
+  if(fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
+
 }
+
+console.log(sum(1, 4)); // 10
+
+/*
+fromN
+fromN + previous from N
+etc.
+stop when fromN === toN
+*/
+
 
 module.exports = sum;
